d8ea8adb0a59cff3f38abdb17a948437ed37bdd5
Lucas <joselucasrs@gmail.com>
2018-11-19 18:08:27 -0200

    Adding Sensores

---
+++ sensores.php
@@ -0,0 +1,51 @@
+<!DOCTYPE html>
+<html lang="pt-BR">
+<head>
+    <meta charset="utf-8">
+
+    <!-- CSS bootstrap -->
+    <link rel="stylesheet" href="css/bootstrap.min.css">
+    <!-- CSS Custom -->
+    <link rel="stylesheet" href="css/style.css">
+    <!-- CSS Font Awesome -->
+    <link rel="stylesheet" href="https://use.fontawesome.com/releases/v5.5.0/css/all.css" integrity="sha384-B4dIYHKNBt8Bc12p+WXckhzcICo0wtJAoU8YZTY5qE0Id1GSseTk6S+L3BlXeVIU" crossorigin="anonymous">
+
+    <title>Prototype Sensores</title>
+</head>
+<body>
+
+<?php include_once "includes/navbar.php"?>
+
+<div class="container col-12">
+    <div class="row col-12" style="padding-top: 20px;">
+
+        <!-- Card Sensor -->
+        <div class="sensores">
+            <div class="nomeSensor">
+                <h3 class="d-inline">Nome do Sensor</h3>
+                <i class="fas fa-battery-full d-inline float-right iconBateria"></i>
+            </div>
+            <div class="corpo">
+                <p>Gráfico</p>
+            </div>
+        </div>
+
+        <!-- Adicionar Grupo -->
+        <div class="sensores">
+            <div class="nomeSensor">
+                <h3 class="d-inline">Nome do Sensor</h3>
+                <i class="fas fa-battery-full d-inline float-right iconBateria"></i>
+            </div>
+            <div class="corpo">
+                <p>Gráfico</p>
+            </div>
+        </div>
+    </div>
+</div>
+
+<!-- Scripts bootstrap -->
+<script src="js/bootstrap.js"></script>
+<script src="js/jquery-3.3.1.slim.min.js"></script>
+<script src="js/popper.min.js"></script>
+</body>
+</html>
